1d116744c691a2d67d8b91b693dc62d10e1a4065
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2022-05-09 15:18:01 -0500

    accept gas_data as arg in run_part

--- src/run_part.F90
+++ src/run_part.F90
@@ -12,19 +12,23 @@ module PyPartMC_run_part
 
   contains
 
-  subroutine py_run_part(arg_run_part_opt) bind(C)
+  subroutine f_run_part(arg_run_part_opt, gas_data_ptr_c) bind(C)
 
     type(scenario_t) :: scenario
     type(env_state_t) :: env_state
     type(aero_data_t) :: aero_data
     type(aero_state_t) :: aero_state
-    type(gas_data_t) :: gas_data
+
+    type(c_ptr), intent(in) :: gas_data_ptr_c
+    type(gas_data_t), pointer :: gas_data_ptr_f => null()
+
     type(gas_state_t) :: gas_state
 
     type(run_part_opt_c) :: arg_run_part_opt
     type(run_part_opt_t) :: run_part_opt
 
-    call run_part(scenario, env_state, aero_data, aero_state, gas_data, gas_state, run_part_opt)
+    call c_f_pointer(gas_data_ptr_c, gas_data_ptr_f)
+    call run_part(scenario, env_state, aero_data, aero_state, gas_data_ptr_f, gas_state, run_part_opt)
 
   end subroutine
 
--- src/run_part.hpp
+++ src/run_part.hpp
@@ -1,12 +1,16 @@
 #pragma once
+#include "gas_data.hpp"
 
 struct run_part_opt_t {
   double t_max, t_output;
 };
 
-extern "C" void py_run_part(run_part_opt_t);
+extern "C" void f_run_part(run_part_opt_t, void*);
 
-void run_part(const run_part_opt_t &run_part_opt) {
-    py_run_part(run_part_opt);
+void run_part(
+    const run_part_opt_t &run_part_opt,
+    const gas_data_t &gas_data
+) {
+    f_run_part(run_part_opt, gas_data.ptr);
 }
 
