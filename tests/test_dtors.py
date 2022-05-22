97e0d8e36c0084ac0dccc424f6c856999cb20d31
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2022-05-22 02:38:28 -0500

    fix test_dtors

--- tests/test_dtors.py
+++ tests/test_dtors.py
@@ -3,12 +3,12 @@ import pytest
 import PyPartMC as ppmc
 
 
-@pytest.mark.parametrize('instance', (
+@pytest.mark.parametrize('sut', (
     pytest.param(ppmc.GasData(("SO2",)), id='GasData'),
     pytest.param(ppmc.AeroData(), id='AeroData'),
     pytest.param(ppmc.GasState(), id='GasState')
 ))
-def test_dtors(sut):
+def test_dtors(sut):  # pytest: disable=unused-argument
     # arrange
     gc.collect()
 
