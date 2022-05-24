dac1a7bc2206f4837083cdb5b972db6cc1e72126
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2022-05-24 14:27:22 -0500

    generalise authorship in license headers

--- tests/test_run_part.py
+++ tests/test_run_part.py
@@ -1,7 +1,7 @@
 ####################################################################################################
 # This file is a part of PyPartMC licensed under the GNU General Public License v3 (LICENSE file)  #
 # Copyright (C) 2022 University of Illinois Urbana-Champaign                                       #
-# Author: Sylwester Arabas                                                                         #
+# Authors: https://github.com/open-atmos/PyPartMC/graphs/contributors                              #
 ####################################################################################################
 
 import PyPartMC as ppmc
