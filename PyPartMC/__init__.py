8856c4d44c4072f249b854aa06a79069312dad66
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2022-06-18 22:25:51 -0500

    cleanup init

--- PyPartMC/__init__.py
+++ PyPartMC/__init__.py
@@ -3,16 +3,11 @@ import os
 from contextlib import contextmanager
 from pathlib import Path
 
-## https://docs.python.org/3/whatsnew/3.8.html#bpo-36085-whatsnew
-#if hasattr(os, 'add_dll_directory'):  # Python 3.8+ on Windows
-#    path = os.path.dirname(__file__)
-#    print(path)
-#    os.add_dll_directory(path)
-
 # https://github.com/diegoferigo/cmake-build-extension/blob/master/src/cmake_build_extension/__init__.py
 @contextmanager
 def build_extension_env():
     cookies = []
+    # https://docs.python.org/3/whatsnew/3.8.html#bpo-36085-whatsnew
     if hasattr(os, "add_dll_directory"):
         for path in os.environ.get("PATH", "").split(os.pathsep):
             if path and Path(path).is_absolute() and Path(path).is_dir():
