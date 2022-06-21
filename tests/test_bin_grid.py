32445ce040ca01b753dc9e13731832a980623a0a
Jeff Curtis <jcurtis2@illinois.edu>
2022-06-21 11:44:12 -0500

    basic test for bin_grid

---
+++ tests/test_bin_grid.py
@@ -0,0 +1,32 @@
+####################################################################################################
+# This file is a part of PyPartMC licensed under the GNU General Public License v3 (LICENSE file)  #
+# Copyright (C) 2022 University of Illinois Urbana-Champaign                                       #
+# Authors: https://github.com/open-atmos/PyPartMC/graphs/contributors                              #
+####################################################################################################
+
+import json
+import PyPartMC as ppmc
+
+BIN_GRID_N_BIN = 100
+
+class TestBinGrid:
+    @staticmethod
+    def test_ctor():
+        # arrange
+        pass
+
+        # act
+        sut = ppmc.BinGrid(BIN_GRID_N_BIN, 1, 1, 100)
+
+        # assert
+        assert sut is not None
+
+    @staticmethod
+    def test_len():
+        # arrange
+        sut = ppmc.BinGrid(BIN_GRID_N_BIN, 1, 1, 100)
+        # act
+        size = len(sut)
+
+        # assert
+        assert size == BIN_GRID_N_BIN
