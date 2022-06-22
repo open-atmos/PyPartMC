005751d7eaae366cc3a715de5a2f419fd3d6d77d
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2022-06-22 17:34:53 -0500

    try debugging the __build_extension_env contextmanager

--- PyPartMC/__init__.py
+++ PyPartMC/__init__.py
@@ -1,5 +1,5 @@
 # pylint: disable=invalid-name,wrong-import-position
-import os
+import os, glob
 from contextlib import contextmanager
 from pathlib import Path
 from collections import namedtuple
@@ -16,6 +16,8 @@ def __build_extension_env():
 
         for path in os.environ.get("PATH", "").split(os.pathsep):
             if path and Path(path).is_absolute() and Path(path).is_dir():
+                print("... adding:", path)
+                print("...... which has:", glob.glob(os.path.join(path, "*.pyd")) 
                 cookies.append(os.add_dll_directory(path))
     try:
         yield
