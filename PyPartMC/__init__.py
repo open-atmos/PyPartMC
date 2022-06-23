9659099887ea4d455c5d7d8ec0eff46cfcb8ba9f
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2022-06-22 23:41:18 -0500

    add dllpath lines back

--- PyPartMC/__init__.py
+++ PyPartMC/__init__.py
@@ -10,6 +10,9 @@ def __build_extension_env():
     cookies = []
     # https://docs.python.org/3/whatsnew/3.8.html#bpo-36085-whatsnew
     if hasattr(os, "add_dll_directory"):
+        basepath = os.path.dirname(os.path.abspath(__file__))
+        dllspath = os.path.join(basepath, '..')
+        os.environ['PATH'] = dllspath + os.pathsep + os.environ['PATH']
         for path in os.environ.get("PATH", "").split(os.pathsep):
             if path and Path(path).is_absolute() and Path(path).is_dir():
                 cookies.append(os.add_dll_directory(path))
