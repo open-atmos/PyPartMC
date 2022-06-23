090e27190f2c5e08f6038690db5486f9572b9d5f
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2022-06-22 19:29:02 -0500

    try with prints again ?!

--- PyPartMC/__init__.py
+++ PyPartMC/__init__.py
@@ -1,5 +1,6 @@
 # pylint: disable=invalid-name,wrong-import-position
 import os
+import glob
 from contextlib import contextmanager
 from pathlib import Path
 from collections import namedtuple
@@ -12,6 +13,7 @@ def __build_extension_env():
     if hasattr(os, "add_dll_directory"):
         for path in os.environ.get("PATH", "").split(os.pathsep):
             if path and Path(path).is_absolute() and Path(path).is_dir():
+                print(path, glob.glob(path))
                 cookies.append(os.add_dll_directory(path))
     try:
         yield
