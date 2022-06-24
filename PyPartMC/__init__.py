0f42d7184a4afd276a53f9326625c054bbf479f1
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2022-06-23 22:11:23 -0500

    try with DLL-Dependencies-Parser

--- PyPartMC/__init__.py
+++ PyPartMC/__init__.py
@@ -19,7 +19,8 @@ def __build_extension_env():
             if path and Path(path).is_absolute() and Path(path).is_dir():
                 dlls = glob.glob(os.path.join(path, '_PyPartMC*'))
                 assert len(dlls) == 1 
-                os.system(f"dumpbin {dlls[0]}")
+                os.system(f"dlldp --pe-file-path {dlls[0]} --results-output-file-path out.json")
+                os.system("cat out.json")
                 ctypes.WinDLL(dlls[0], winmode=0)
                 ctypes.cdll.LoadLibrary(dlls[0])
                 cookies.append(os.add_dll_directory(path))
