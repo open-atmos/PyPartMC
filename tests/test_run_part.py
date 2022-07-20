e37567aea7fe1d97716ede126d1305816b31d175
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2022-07-20 18:30:03 +0200

    make pylint happy

--- tests/test_run_part.py
+++ tests/test_run_part.py
@@ -16,6 +16,7 @@ from .test_aero_state import AERO_STATE_CTOR_ARG_MINIMAL
 class TestRunPart:
     @staticmethod
     def test_args():
+        # pylint: disable=unused-variable
         # arrange
         env_state = ppmc.EnvState(ENV_STATE_CTOR_ARG_MINIMAL)
         aero_data = ppmc.AeroData(AERO_DATA_CTOR_ARG_MINIMAL)
@@ -26,7 +27,10 @@ class TestRunPart:
         run_part_opt = ppmc.RunPartOpt(RUN_PART_OPT_CTOR_ARG_MINIMAL)
 
         # act
-        # ppmc.run_part(scenario, env_state, aero_data, aero_state, gas_data, gas_state, run_part_opt)
+        # TODO #33
+        # ppmc.run_part(
+        #     scenario, env_state, aero_data, aero_state, gas_data, gas_state, run_part_opt
+        # )
 
         # assert
         pass
