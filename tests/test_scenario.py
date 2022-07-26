45632ed59777d2d4387141493c9397c4e6f62a54
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2022-07-26 14:25:08 +0200

    set chamber loss function params in test_loss_rate

--- tests/test_scenario.py
+++ tests/test_scenario.py
@@ -10,6 +10,7 @@ import json
 import pytest
 import numpy as np
 import PyPartMC as ppmc
+from PyPartMC import si
 
 from .test_gas_data import GAS_DATA_CTOR_ARG_MINIMAL
 from .test_aero_data import AERO_DATA_CTOR_ARG_MINIMAL
@@ -121,20 +122,26 @@ class TestScenario:
         assert json_actual == SCENARIO_CTOR_ARG_MINIMAL
 
     @staticmethod
-    @pytest.mark.parametrize("loss_function_param", (
-        'invalid',
-        'none',
-        'constant',
-        'volume',
-        'drydep',
-        'chamber'))
-    def test_loss_rate(loss_function_param:str):
+    @pytest.mark.parametrize("loss_function_params", (
+        {'loss_function': 'none'},
+        {'loss_function': 'constant'},
+        {'loss_function': 'volume'},
+        {'loss_function': 'drydep'},
+        {
+            'loss_function': 'chamber',
+            'chamber_vol': 84.3 * si.m**3,
+            'area_diffuse': 103 * si.m**2,
+            'area_sedi': 12.6 * si.m**2,
+            'prefactor_BL': 0.005 * si.m,
+            'exponent_BL': 0.274
+        }
+    ))
+    def test_loss_rate(loss_function_params:str):
         # arrange
         aero_data = ppmc.AeroData(AERO_DATA_CTOR_ARG_MINIMAL)
         env_state = ppmc.EnvState(ENV_STATE_CTOR_ARG_MINIMAL)
         gas_data  = ppmc.GasData(GAS_DATA_CTOR_ARG_MINIMAL)
-        scenario_ctor_arg = SCENARIO_CTOR_ARG_MINIMAL
-        scenario_ctor_arg["loss_function"] = loss_function_param
+        scenario_ctor_arg = {**SCENARIO_CTOR_ARG_MINIMAL, **loss_function_params}
         scenario = ppmc.Scenario(gas_data, aero_data, scenario_ctor_arg)
         vol =  (4/3)*np.pi*(1e-6)**3
         density = 1
