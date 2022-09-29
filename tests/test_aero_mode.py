b9bfde2c951f3b3c4e1a3370eb06fcec6e059606
Jeff Curtis <jcurtis2@illinois.edu>
2022-09-29 16:53:10 -0500

    add first test for aero_mode

---
+++ tests/test_aero_mode.py
@@ -0,0 +1,23 @@
+####################################################################################################
+# This file is a part of PyPartMC licensed under the GNU General Public License v3 (LICENSE file)  #
+# Copyright (C) 2022 University of Illinois Urbana-Champaign                                       #
+# Authors: https://github.com/open-atmos/PyPartMC/graphs/contributors                              #
+####################################################################################################
+
+import numpy as np
+
+import PyPartMC as ppmc
+
+from .test_aero_data import AERO_DATA_CTOR_ARG_MINIMAL
+
+class TestAeroMode:
+    @staticmethod
+    def test_ctor():
+        # arrange
+        aero_data = ppmc.AeroData(AERO_DATA_CTOR_ARG_MINIMAL)
+
+        # act
+        sut = ppmc.AeroMode(aero_data)
+
+        # assert
+        assert sut is not None
