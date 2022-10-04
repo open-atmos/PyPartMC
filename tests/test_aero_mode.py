68577115e16913eae450cbb45110f255d7c62da9
Jeff Curtis <jcurtis2@illinois.edu>
2022-10-04 14:14:30 -0500

    add more tests for aero_mode

--- tests/test_aero_mode.py
+++ tests/test_aero_mode.py
@@ -30,3 +30,46 @@ class TestAeroMode:
         sut.vol_frac = vals
 
         assert sut.vol_frac == vals
+
+    @staticmethod
+    def test_set_num_conc():
+        aero_data = ppmc.AeroData(AERO_DATA_CTOR_ARG_MINIMAL)
+        # act
+        sut = ppmc.AeroMode(aero_data)
+        val = 1234
+        sut.num_conc = val
+
+        assert sut.num_conc == val
+
+    @staticmethod
+    def test_set_gsd():
+        aero_data = ppmc.AeroData(AERO_DATA_CTOR_ARG_MINIMAL)
+        # act
+        sut = ppmc.AeroMode(aero_data)
+        val = 1.234
+        sut.gsd = val
+
+        assert sut.gsd == val
+
+    @staticmethod
+    def test_set_char_radius():
+        aero_data = ppmc.AeroData(AERO_DATA_CTOR_ARG_MINIMAL)
+        # act
+        sut = ppmc.AeroMode(aero_data)
+        val = 1e-9
+        sut.char_radius = val
+
+        assert sut.char_radius == val
+
+    @staticmethod
+    def test_set_vol_frac_invalid():
+        # act
+        aero_data = ppmc.AeroData(AERO_DATA_CTOR_ARG_MINIMAL)
+        # act
+        sut = ppmc.AeroMode(aero_data)
+        try:
+            sut.vol_frac = [1.0, 0.0]
+        except RuntimeError:
+            return
+
+        assert False
