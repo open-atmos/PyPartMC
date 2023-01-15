1c7cb00700c352221e1c88e003b9c48e9ad88ea8
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2023-01-14 22:55:06 -0600

    making rand_particle a method rather than attribute as it does more than just returning an attribute

--- tests/test_aero_state.py
+++ tests/test_aero_state.py
@@ -174,7 +174,7 @@ class TestAeroState:
     @staticmethod
     def test_get_random_particle(sut_minimal):  # pylint: disable=redefined-outer-name
         # act
-        particle = sut_minimal.rand_particle
+        particle = sut_minimal.rand_particle()
 
         # assert
         assert isinstance(particle, ppmc.AeroParticle)
