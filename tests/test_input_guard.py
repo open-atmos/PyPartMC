9f387e708e4079dbc0c920f179f9642270987a3d
Griger5 <griger04@gmail.com>
2025-03-13 16:39:43 +0100

    Fix: pre-commit hooks

--- tests/test_input_guard.py
+++ tests/test_input_guard.py
@@ -1,4 +1,5 @@
 import copy
+
 import pytest
 
 import PyPartMC as ppmc
@@ -9,6 +10,7 @@ from .test_aero_mode import AERO_MODE_CTOR_LOG_NORMAL, AERO_MODE_CTOR_SAMPLED
 from .test_gas_data import GAS_DATA_CTOR_ARG_MINIMAL
 from .test_run_part_opt import RUN_PART_OPT_CTOR_ARG_MINIMAL
 from .test_scenario import SCENARIO_CTOR_ARG_MINIMAL
+
 
 class TestInputGuard:
     @staticmethod
@@ -23,7 +25,10 @@ class TestInputGuard:
             ppmc.AeroDist(aero_data, [mode])
 
         # assert
-        assert str(exc_info.value) == "WARNING: \"test_mode/not_used\" parameter remains unused."
+        assert (
+            str(exc_info.value)
+            == 'WARNING: "test_mode/not_used" parameter remains unused.'
+        )
 
     @staticmethod
     def test_aero_mode_unused_parameter():
@@ -37,7 +42,10 @@ class TestInputGuard:
             ppmc.AeroMode(aero_data, mode)
 
         # assert
-        assert str(exc_info.value) == "WARNING: \"test_mode/not_used\" parameter remains unused."
+        assert (
+            str(exc_info.value)
+            == 'WARNING: "test_mode/not_used" parameter remains unused.'
+        )
 
     @staticmethod
     def test_env_state_unused_parameter():
@@ -50,47 +58,50 @@ class TestInputGuard:
             ppmc.EnvState(args)
 
         # assert
-        assert str(exc_info.value) == "WARNING: \"not_used\" parameter remains unused."
+        assert str(exc_info.value) == 'WARNING: "not_used" parameter remains unused.'
 
     @staticmethod
     def test_run_part_opt_unused_parameter():
-        #arrange
+        # arrange
         args = copy.deepcopy(RUN_PART_OPT_CTOR_ARG_MINIMAL)
         args["not_used"] = 0
 
-        #act
+        # act
         with pytest.raises(RuntimeError) as exc_info:
             ppmc.RunPartOpt(args)
 
-        assert str(exc_info.value) == "WARNING: \"not_used\" parameter remains unused."
+        assert str(exc_info.value) == 'WARNING: "not_used" parameter remains unused.'
 
     @staticmethod
     def test_scenario_unused_parameter():
-        #arrange
+        # arrange
         aero_data = ppmc.AeroData(AERO_DATA_CTOR_ARG_MINIMAL)
         gas_data = ppmc.GasData(GAS_DATA_CTOR_ARG_MINIMAL)
         args = copy.deepcopy(SCENARIO_CTOR_ARG_MINIMAL)
         args["not_used"] = 0
 
-        #act
+        # act
         with pytest.raises(RuntimeError) as exc_info:
             ppmc.Scenario(gas_data, aero_data, args)
 
-        assert str(exc_info.value) == "WARNING: \"not_used\" parameter remains unused."
+        assert str(exc_info.value) == 'WARNING: "not_used" parameter remains unused.'
 
     @staticmethod
     def test_nested_unused_parameter():
         # arrange
         aero_data = ppmc.AeroData(AERO_DATA_CTOR_ARG_MINIMAL)
         mode = copy.deepcopy(AERO_MODE_CTOR_SAMPLED)
-        mode["test_mode"]["size_dist2"] = [{"diam" : [1]}, {"num_conc" : [100]}]
+        mode["test_mode"]["size_dist2"] = [{"diam": [1]}, {"num_conc": [100]}]
 
         # act
         with pytest.raises(RuntimeError) as exc_info:
             ppmc.AeroDist(aero_data, [mode])
 
         # assert
-        assert str(exc_info.value) == "WARNING: \"test_mode/size_dist2/diam\" parameter remains unused."
+        assert (
+            str(exc_info.value)
+            == 'WARNING: "test_mode/size_dist2/diam" parameter remains unused.'
+        )
 
     @staticmethod
     def test_same_name_unused_parameter():
@@ -104,6 +115,9 @@ class TestInputGuard:
         # act
         with pytest.raises(RuntimeError) as exc_info:
             ppmc.AeroDist(aero_data, [modes])
-        
+
         # assert
-        assert str(exc_info.value) == "WARNING: \"sampled_mode/geom_mean_diam\" parameter remains unused."
+        assert (
+            str(exc_info.value)
+            == 'WARNING: "sampled_mode/geom_mean_diam" parameter remains unused.'
+        )
