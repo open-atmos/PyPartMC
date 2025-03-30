d1f2ef9f26134771c0166da9b186eaf4a86d0135
Jeffrey Curtis <jcurtis2@illinois.edu>
2025-03-30 16:26:23 -0500

    start to add input_sectional

--- src/output.F90
+++ src/output.F90
@@ -92,4 +92,42 @@ contains
 
   end subroutine
 
+  subroutine f_input_sectional(filename_data, filename_size, index, time, del_t, &
+       bin_grid_ptr_c, aero_data_ptr_c, aero_binned_ptr_c, gas_data_ptr_c, &
+       gas_state_ptr_c, env_state_ptr_c) bind(C)
+
+    type(aero_binned_t), pointer :: aero_binned_ptr_f => null()
+    type(aero_data_t), pointer :: aero_data_ptr_f => null()
+    type(bin_grid_t), pointer :: bin_grid_ptr_f => null()
+    type(env_state_t), pointer :: env_state_ptr_f => null()
+    type(gas_state_t), pointer :: gas_state_ptr_f => null()
+    type(gas_data_t), pointer :: gas_data_ptr_f => null()
+    character(kind=c_char), dimension(*), intent(in) :: filename_data
+    integer(c_int), intent(in) :: filename_size
+    type(c_ptr) :: aero_data_ptr_c, aero_state_ptr_c, gas_data_ptr_c, &
+         gas_state_ptr_c, env_state_ptr_c, aero_binned_ptr_c, bin_grid_ptr_c
+    integer(c_int), intent(out) :: index
+    real(c_double), intent(out) :: time, del_t
+    character(len=PMC_UUID_LEN) :: uuid
+
+    character(len=filename_size) :: filename
+    integer :: i
+
+    do i=1, filename_size
+       filename(i:i) = filename_data(i)
+    end do
+
+    call c_f_pointer(aero_data_ptr_c, aero_data_ptr_f)
+    call c_f_pointer(aero_binned_ptr_c, aero_binned_ptr_f)
+    call c_f_pointer(bin_grid_ptr_c, bin_grid_ptr_f)
+    call c_f_pointer(gas_data_ptr_c, gas_data_ptr_f)
+    call c_f_pointer(gas_state_ptr_c, gas_state_ptr_f)
+    call c_f_pointer(env_state_ptr_c, env_state_ptr_f)
+
+    call input_sectional(filename, index, time, del_t, uuid, bin_grid_ptr_f, &
+         aero_data_ptr_f, aero_binned_ptr_f, gas_data_ptr_f, gas_state_ptr_f, &
+         env_state_ptr_f)
+
+  end subroutine
+
 end module
