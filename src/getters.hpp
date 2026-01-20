cf525c0abc4034d08a06ba35b8e46c6c4f9403b5
Jeffrey Curtis <jcurtis2@illinois.edu>
2026-01-20 10:12:22 -0600

    add comments. change to inline. formatting

--- src/getters.hpp
+++ src/getters.hpp
@@ -9,20 +9,23 @@
 #include <utility>
 #include <valarray>
 
-namespace pypartmc { // possible namespace to avoid name conflicts
+namespace pypartmc {
 
+// Retrieves a single value from a Fortran routine.
 template <typename T = double, typename SelfType, typename Func>
-inline T get_value(const SelfType &self, Func func) { // inline to keep it header-only
+inline T get_value(const SelfType &self, Func func) {
     T value{};
     func(self.ptr.f_arg(), &value);
     return value;
 }
 
+// Sets a single value through a Fortran routine via input parameter.
 template <typename T = double, typename SelfType, typename Func>
 inline void set_value(SelfType &self, Func func, T value) {
     func(self.ptr.f_arg_non_const(), &value);
 }
 
+// Retrieves a computed value from a Fortran routine with additional arguments.
 template <typename T = double, typename SelfType, typename Func, typename... Args>
 inline T get_derived_value(const SelfType &self, Func func, Args&&... args) {
     T value{};
@@ -30,21 +33,23 @@ inline T get_derived_value(const SelfType &self, Func func, Args&&... args) {
     return value;
 }
 
+// Retrieves an array from a Fortran routine, querying length dynamically via len_fn.
 template<typename T = double, typename SelfType, typename Func, typename LenF, typename... ExtraArgs>
-static auto get_array_values(const SelfType &self, Func f, LenF len_fn,
+inline auto get_array_values(const SelfType &self, Func f, LenF len_fn,
     ExtraArgs&&... extra) {
-        int len;
-        len_fn(self.ptr.f_arg(), &len);
-        std::valarray<T> arr(len);
-        f(self.ptr.f_arg(), std::forward<ExtraArgs>(extra)..., begin(arr), &len);
-        return arr;
-    }
+    int len;
+    len_fn(self.ptr.f_arg(), &len);
+    std::valarray<T> arr(len);
+    f(self.ptr.f_arg(), std::forward<ExtraArgs>(extra)..., begin(arr), &len);
+    return arr;
+}
 
+// Retrieves an array from a Fortran routine with a predetermined length.
 template<typename T = double, typename SelfType, typename Func, typename... ExtraArgs>
-static auto get_array_values_set_len(const SelfType &self, Func f, int len,
+inline auto get_array_values_set_len(const SelfType &self, Func f, int len,
     ExtraArgs&&... extra) {
-        std::valarray<T> arr(len);
-        f(self.ptr.f_arg(), std::forward<ExtraArgs>(extra)..., begin(arr), &len);
-        return arr;
-    }
+    std::valarray<T> arr(len);
+    f(self.ptr.f_arg(), std::forward<ExtraArgs>(extra)..., begin(arr), &len);
+    return arr;
+}
 }
