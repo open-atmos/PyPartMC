010416ee84f379be69a5ff1e07a690a646004a64
orzel320 <52106515+orzel320@users.noreply.github.com>
2026-06-10 02:58:08 +0200

    Added #pragma_once to input_guard.hpp, corrected si.hpp generation, implemented PyPartMC.hpp generation for all .hpp libraries, changed paths CMakeLists.txt in readme_cpp uses to pull libraries, finished test.cpp

--- readme_cpp/test.cpp
+++ readme_cpp/test.cpp
@@ -1,7 +1,52 @@
-#include "bin_grid.hpp"
-#include <iostream>
+#include "PyPartMC.hpp"
+#include "PyPartMC/si.hpp"
 
-int main(){
-    BinGrid a;
-    std::cout << "Success\n";
+int main() {
+    auto aero_data = std::shared_ptr<AeroData>(new AeroData({
+        { {"OC", {1000.0 * si::kg / (si::m * si::m * si::m), 0.0, 1e-3 * si::kg / si::mol, 0.001, 0.0, 0.0}} },
+        { {"BC", {1800.0 * si::kg / (si::m * si::m * si::m), 0.0, 1e-3 * si::kg / si::mol, 0.0,   0.0, 0.0}} }
+    }));
+
+    auto aero_dist = AeroDist(
+        aero_data, 
+        nlohmann::ordered_json::array({
+            nlohmann::ordered_json::object({
+                {"cooking", {
+                    {"mass_frac", nlohmann::ordered_json::array({ nlohmann::ordered_json::object({{"OC", {1.0}}}) }) }, 
+                    {"diam_type", "geometric"},
+                    {"mode_type", "log_normal"},
+                    {"num_conc", 3200.0 / (si::cm * si::cm * si::cm)},
+                    {"geom_mean_diam", 8.64 * si::nm},
+                    {"log10_geom_std_dev", 0.28}
+                }},
+                {"diesel", {
+                    {"mass_frac", nlohmann::ordered_json::array({ nlohmann::ordered_json::object({{"OC", {0.3}}}), nlohmann::ordered_json::object({{"BC", {0.7}}}) }) }, 
+                    {"diam_type", "geometric"},
+                    {"mode_type", "log_normal"},
+                    {"num_conc", 2900.0 / (si::cm * si::cm * si::cm)},
+                    {"geom_mean_diam", 50.0 * si::nm},
+                    {"log10_geom_std_dev", 0.24}
+                }}
+            })
+        })
+    );
+
+    int n_part = 100;
+    auto aero_state = AeroState(aero_data, n_part, "nummass_source");
+    
+    AeroState::dist_sample(aero_state, aero_dist, 1.0, 0.0, true, true);
+
+    auto masses = AeroState::masses(aero_state, {}, {});
+    auto num_concs = AeroState::num_concs(aero_state);
+
+    double total_mass = std::inner_product(
+        std::begin(num_concs), std::end(num_concs), 
+        std::begin(masses), 
+        0.0
+    );
+    
+    std::cout.precision(15);
+    std::cout << std::scientific << total_mass << " # kg/m3\n";
+
+    return 0;
 }
---
+++ src/PyPartMC/__generate_si.py
@@ -0,0 +1,36 @@
+from collections import namedtuple
+
+def __generate_si():
+    prefixes = {
+        "T": 1e12,
+        "G": 1e9,
+        "M": 1e6,
+        "k": 1e3,
+        "h": 1e2,
+        "da": 1e1,
+        "": 1e0,
+        "d": 1e-1,
+        "c": 1e-2,
+        "m": 1e-3,
+        "u": 1e-6,
+        "n": 1e-9,
+        "p": 1e-12,
+    }
+    units = {
+        "m": 1e0,
+        "g": 1e-3,
+        "s": 1e0,
+        "K": 1e0,
+        "Pa": 1e0,
+        "mol": 1e0,
+        "W": 1e0,
+        "J": 1e0,
+        "N": 1e0,
+    }
+    return namedtuple("SI", [prefix + unit for prefix in prefixes for unit in units])(
+        **{
+            prefix_k + unit_k: prefix_v * unit_v
+            for prefix_k, prefix_v in prefixes.items()
+            for unit_k, unit_v in units.items()
+        }
+    )
